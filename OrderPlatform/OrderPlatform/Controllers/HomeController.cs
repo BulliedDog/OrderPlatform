02dd17d34cbe92bebf1b52438ae5fbfe708fb243
BulliedDog <albertorovai1@gmail.com>
2021-03-08 13:06:19 +0100

    commit prova

--- OrderPlatform/OrderPlatform/Controllers/HomeController.cs
+++ OrderPlatform/OrderPlatform/Controllers/HomeController.cs
@@ -20,6 +20,7 @@ namespace OrderPlatform.Controllers
             return View();
         }
 
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";

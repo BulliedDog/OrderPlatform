9e34873482012da80ef498700f2fb0935a02b531
BulliedDog <albertorovai1@gmail.com>
2021-03-10 11:32:08 +0100

    commit

--- OrderPlatform/OrderPlatform/Controllers/StateController.cs
+++ OrderPlatform/OrderPlatform/Controllers/StateController.cs
@@ -1,4 +1,5 @@
-﻿using OrderPlatform.Services;
+﻿using OrderPlatform.Models;
+using OrderPlatform.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +11,24 @@ namespace OrderPlatform.Controllers
     public class StateController : Controller
     {
         public StateService service = new StateService();
-        // GET: State
+        
+        [HttpGet]
         public ActionResult Index()
         {
             return View(service.GetList());
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(service.Get(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(StateModel model)
+        {
+            service.Set(model);
+            return RedirectToAction("Index");
         }
     }
 }
